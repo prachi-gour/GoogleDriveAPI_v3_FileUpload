d2df657eeef4bd3019b18645ef96b50e816c79c5
Prachi Gour <66124312+PrachiGour2599@users.noreply.github.com>
2020-06-28 21:33:53 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using GoogleDriveFilesAPI_v3.Models;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace GoogleDriveFilesAPI_v3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetGoogleDriveFiles()
+        {
+            return View(GoogleDriveFilesRespository.GetDriveFiles());
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFile(GoogleDriveFiles file)
+        {
+            GoogleDriveFilesRespository.DeleteFile(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            GoogleDriveFilesRespository.FileUpload(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+
+        public void DownloadFile(string id)
+        {
+            string FilePath = GoogleDriveFilesRespository.DownloadGoogleFile(id);
+
+
+            Response.ContentType = "application/zip";
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(FilePath));
+            Response.WriteFile(System.Web.HttpContext.Current.Server.MapPath("~/GoogleDriveFiles/" + Path.GetFileName(FilePath)));
+            Response.End();
+            Response.Flush();
+        }
+    }
+} 
